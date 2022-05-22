864b699067f30cc6a03a5c4297a20d9c4bf24182
Airon Miranda <92761591+oaironmiranda@users.noreply.github.com>
2022-05-22 13:18:13 -0300

    version-3

--- main.py
+++ main.py
@@ -2,98 +2,71 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.list import ThreeLineListItem
 from datetime import datetime
-import psycopg2
-
-
+from mysql.connector
 
 class MainApp(MDApp):
-    def build(self):
-        # I hide my informations just for safe reasons
-        conn = psycopg2.connect(
-            host='host',
-            database='database',
-            user='user',
-            password='password',
-            port='5432'
-        )
 
-        c = conn.cursor()
-        c.execute("""CREATE TABLE if not exists glic (
-            dia text,
-            hora text,
-            glicemia integer);
-            """)
-        conn.commit()
-        conn.close()
+	# database connection function
+	def connector(self, command, value):
+		conn = mysql.connector.connect(
+			host='b6uvkfjwjuqi6ckzxz5s-mysql.services.clever-cloud.com',
+			database='b6uvkfjwjuqi6ckzxz5s',
+			user='uczx3ztt4o1jmssx',
+			password='J57IrbhnsqCGnReZdnHx',
+			port='3306')
 
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "DeepPurple"
-        return Builder.load_file('main.kv')
+		c = conn.cursor()
+		c.execute(command, value)
+		conn.commit()
+		conn.close()
 
-    def calc(self):
-        if str(self.root.ids.num.text) == '':
-            self.root.ids.label.text = "Try Again"
-        else:
-            a = int(self.root.ids.num.text)
-            if a < 190:
-                self.root.ids.label.text = "You don't need to Add more insuline"
-                if a <= 85:
-                    self.root.ids.label.text = "You need to fix your blood sugar"
-            elif a >= 190:
-                b = (a - 120) / 70
-                self.root.ids.label.text = f'Add {round(b)}UI'
+	# time function
+	def time(self):
+		data = datetime.strftime(datetime.today(), "%d/%m/%y")
+		hora = datetime.strftime(datetime.now(), "%H:%M")
 
-        conn = psycopg2.connect(
-            host='host',
-            database='database',
-            user='user',
-            password='password',
-            port='5432'
-        )
+	# app builder function
+	def build(self):
+		self.theme_cls.theme_style = "Dark"
+		self.theme_cls.primary_palette = "DeepPurple"
 
-        data = datetime.strftime(datetime.today(), "%d/%m/%y")
-        hora = datetime.strftime(datetime.now(), "%H:%M")
+		self.theme_cls.theme_style = "Dark"
+		self.theme_cls.primary_palette = "DeepPurple"
 
-        # create a cursor
-        c = conn.cursor()
-        sql_command = "INSERT INTO glic (dia, hora, glicemia) VALUES (%s, %s, %s)"
-        values = (f"{data}", f"{hora}", int(self.root.ids.num.text),)
+		return Builder.load_file('main.kv')
 
-        # Add a record
-        c.execute(sql_command, values)
+	# calculation function
+	def calc(self):
+		if str(self.root.ids.num.text) == '':
+			self.root.ids.label.text = "TRY AGAIN"
+		else:
+			a = int(self.root.ids.num.text)
+			if a < 190:
+				self.root.ids.label.text = "YOU DON'T NEED TO FIX YOUR BLOOD SUGAR"
+				if a <= 85:
+					self.root.ids.label.text = "YOU NEED TO FIX YOUR BLOOD SUGAR"
+			elif a >= 190:
+				b = (a - 120) / 70
+				self.root.ids.label.text = f"ADD {round(b)} UI"
 
-        # commit the changes
-        conn.commit()
+		command = "INSERT INTO glic (dia, hora, glicemia) VALUES (%s, %s, %s)"
+		value = (f"{self.time.data()}", f"{self.time.hora()}", int(self.root.ids.num.text),)
 
-        # close or conection
-        conn.close()
+		self.connector(command, value)
 
-    def navigation_draw(self):
-        if self.theme_cls.theme_style == "Dark":
-            self.theme_cls.theme_style = "Light"
-        elif self.theme_cls.theme_style == "Light":
-            self.theme_cls.theme_style = "Dark"
+		self.on_start()
 
-    def on_start(self):
-        conn = psycopg2.connect(
-            host='host',
-            database='database',
-            user='user',
-            password='password',
-            port='5432'
-        )
-        c = conn.cursor()
+	# database show function
+	def on_start(self):
+		command = "SELECT * FROM glic WHERE dia = %s ORDER BY id DESC"
+		value = (f{self.time.data()})
+		c = self.connector(command, value)
 
-        c.execute("SELECT  * FROM glic")
-        records = c.fetchall()
+		records = c.fetchall()
 
-        for record in records:
-            self.root.ids.container.add_widget(
-                ThreeLineListItem(text=f"{record[2]}", secondary_text=f"{record[0]}", tertiary_text=f"{record[1]}")
-            )
-
-        conn.commit()
-        conn.close()
-
+		for record in records:
+			self.root.ids.container.add_widget(
+				ThreeLineListItem(text=f"{record[3]}", secondary_text=f"{record[1]}", tertiary_text=f"{record[2]}")
+			)
 
 MainApp().run()
